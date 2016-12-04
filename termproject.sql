f04e136601755d8b6bb0d6e380cf457d554034b7
Felix Huang <felixthe8@gmail.com>
2016-12-03 20:46:48 -0800

    Update termproject.sql

--- termproject.sql
+++ termproject.sql
@@ -814,6 +814,12 @@ UNION
 	NATURAL JOIN customer;
 GRANT SELECT ON customer_address_v TO public;
 
+/*3. Mechanic_Mentor_v - reports all of the mentor/mentee relationships, sorted by the name of the mentor, thne the mentee.*/
+CREATE VIEW mechanic_mentorship_v AS
+SELECT  DISTINCT (m1.eID) AS 'Student ID' , (SELECT ename from employee where m1.eID = employee.eID) AS 'Student Name' , (m2.mentorID) AS 'Mentor ID', (SELECT ename from employee where m2.mentorID = employee.eID) AS 'Mentor Name' from employee
+JOIN mentorship m1 on employee.eID = m1.eID
+JOIN mentorship m2 on employee.eID = m2.eID;
+
 /* 5 5.	Prospective_resurrection_v – List all of the prospective customers 
 who have had three or more contacts, and for whom the most recent contact 
 was more than a year ago.  They might be ripe for another attempt.
